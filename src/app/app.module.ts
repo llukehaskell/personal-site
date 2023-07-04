82cd2ab383a1177abf08e741e73f63ef53dbb827
llukehaskell <lukehaskell@gmail.com>
2023-07-04 13:51:26 -0400

    added routing & toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { ConnectComponent } from './connect/connect.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
 
+    RouterModule.forRoot([
+      {path: 'connect', component: 
+    ConnectComponent}
+    ]),
+
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
