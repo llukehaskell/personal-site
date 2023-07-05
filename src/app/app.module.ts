707328c6422c170d023d194221ea531716044c9e
llukehaskell <lukehaskell@gmail.com>
2023-07-05 15:24:13 -0400

    finihsed toolbar, polished website, added home & hire-me pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { ConnectComponent } from './connect/connect.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './home/home.component';
+import { HireMeComponent } from './hire-me/hire-me.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnectComponent
+    ConnectComponent,
+    HomeComponent,
+    HireMeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
 
     RouterModule.forRoot([
-      {path: 'connect', component: 
-    ConnectComponent}
+      {path: 'home', component: HomeComponent},
+      {path: 'connect', component: ConnectComponent},
+      {path: 'hire-me', component: HireMeComponent},
+      
+      {path: '', redirectTo: '/home', pathMatch: 'full'}, // makes home the default page
     ]),
 
     BrowserAnimationsModule,
